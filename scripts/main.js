96980283382c6712c251389f06b64f2bcd3149c8
Gerard Beckerleg <gerard.beckerleg@orchard.com.au>
2018-06-06 11:14:23 +1000

    add remaining logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,79 @@
-var currentPoints = 30;
-
+var currentPoints = 0;
+var targetPoints = 36;
+document.getElementById("points").innerHTML = targetPoints;
+document.getElementById("actualpoints").innerHTML = currentPoints;
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-    var source = ev.target.parentNode.getAttribute("data-card-type");
-    ev.dataTransfer.setData("type", source);
+  ev.dataTransfer.setData("text", ev.target.id);
+  var source = ev.target.parentNode.getAttribute("data-card-type");
+  ev.dataTransfer.setData("type", source);
 }
 
 function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    console.log(ev.dataTransfer);
-    var target = ev.target.getAttribute("data-card-type");
-    console.log(target);
-        var source = ev.dataTransfer.getData("type");
-        console.log(source);
-        if (target == "semester" && target != source) {
-        currentPoints = currentPoints - 10;
-    } else if(target=="module") {
-        currentPoints = currentPoints + 10;
-    }
-    if (currentPoints==0){
-        var element = document.getElementById("points");
-    element.classList.remove("is-danger");
-    element.classList.add("is-success");
-    element.innerHTML = "Pass"
-    } else {
-        var element = document.getElementById("points");
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  var target = ev.target.getAttribute("data-card-type");
+  var source = ev.dataTransfer.getData("type");
+  if (target == "semester" && target != source) {
+    currentPoints = currentPoints + 6;
+  } else if (target == "course" && target != source) {
+    currentPoints = currentPoints - 6;
+  }
+  calcpoints();
+  calcsemester();
+}
+function calcpoints(){
+    if (currentPoints >= targetPoints) {
+        var element = document.getElementById("requiredpoints");
+        element.classList.remove("is-danger");
+        element.classList.add("is-success");
+        element.innerHTML = "Pass";
+      } else {
+        var element = document.getElementById("requiredpoints");
         element.classList.remove("is-success");
         element.classList.add("is-danger");
-        element.innerHTML = "Fail"
-    }
-    console.log(currentPoints);
+        element.innerHTML = "Fail";
+      }
+      document.getElementById("actualpoints").innerHTML = currentPoints;
 }
+function calcsemester(){
+
+    var semester1 = document.querySelector("[data-semester-1]").querySelectorAll("[data-course]").length;
+    var semester2 = document.querySelector("[data-semester-2]").querySelectorAll("[data-course]").length;
+    var semester3 = document.querySelector("[data-semester-3]").querySelectorAll("[data-course]").length;
+    if(semester1 > 0 && semester2 > 0 && semester3 > 0){
+        var element = document.getElementById("semester");
+        element.classList.remove("is-danger");
+        element.classList.add("is-success");
+        element.innerHTML = "Pass";
+    }
+    else{
+        var element = document.getElementById("semester");
+        element.classList.remove("is-success");
+        element.classList.add("is-danger");
+        element.innerHTML = "Fail";
+    }
+}
+
+var rad = document.myForm.studenttype;
+    var prev = null;
+    for(var i = 0; i < rad.length; i++) {
+        rad[i].onclick = function() {
+            
+            if(this !== prev) {
+                prev = this;
+            }
+            if(this.value=="domestic"){
+                targetPoints = 36;
+                document.getElementById("points").innerHTML = targetPoints;
+            }else{
+                targetPoints = 48;
+                document.getElementById("points").innerHTML = targetPoints;
+            }
+            calcpoints();
+        };
+    }
